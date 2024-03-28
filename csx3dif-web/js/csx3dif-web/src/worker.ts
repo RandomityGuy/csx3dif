b6342fdc366f3e7330e06db0b714d095d6d01128
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2024-03-28 14:52:54 +0530

    get it to work on vite again

--- csx3dif-web/js/csx3dif-web/src/worker.ts
+++ csx3dif-web/js/csx3dif-web/src/worker.ts
@@ -1,19 +1,16 @@
 import init, { convert_csx, initThreadPool, init_panic_hook } from "csx3dif-web";
 
-init().then(() => initThreadPool(navigator.hardwareConcurrency).then(() => {
-    init_panic_hook();
-    main();
-}));
+await init();
+await initThreadPool(navigator.hardwareConcurrency);
+init_panic_hook();
 
-const main = () => {
-    // Receive messages
-    addEventListener("message", async (event) => {
-        let [f, engine, version, mb, bsp, ptep, plep] = event.data;
-        let csxfile = await f.text();
-        let convert_results = convert_csx(csxfile, engine, version, mb, bsp, ptep, plep, (current: number, total: number, status: string, finishStatus: string) => {
-            // console.log(`${current} / ${total} - ${status} - ${finishStatus}`);
-            postMessage([0, current, total, status, finishStatus]);
-        });
-        postMessage([1, convert_results]);
+// Receive messages
+addEventListener("message", async (event) => {
+    let [f, engine, version, mb, bsp, ptep, plep] = event.data;
+    let csxfile = await f.text();
+    let convert_results = convert_csx(csxfile, engine, version, mb, bsp, ptep, plep, (current: number, total: number, status: string, finishStatus: string) => {
+        // console.log(`${current} / ${total} - ${status} - ${finishStatus}`);
+        postMessage([0, current, total, status, finishStatus]);
     });
-}
+    postMessage([1, convert_results]);
+});
---
+++ csx3dif-web/js/csx3dif-web/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
+
+export default defineConfig({
+  plugins: [
+    wasm(),
+    topLevelAwait(),
+    crossOriginIsolation()
+  ],
+  server: {
+    fs: {
+      allow: ["../../pkg", "src"]
+    }
+  },
+  worker: {
+    format: 'es'
+  }
+});
